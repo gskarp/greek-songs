bd7e4b2aa8e7c3bc3ed59b9356725f9e60a38c8e
Yannis Skarpelos <gskarp@otenet.gr>
2025-02-27 01:07:21 +0200

    Update greeksongs2.py

--- greeksongs2.py
+++ greeksongs2.py
@@ -22,9 +22,7 @@ st.set_page_config(layout="wide", page_title="Greek Songs Dashboard", initial_si
 st.markdown(""" <style> body { background-color: #121212; color: white; } </style> """, unsafe_allow_html=True)
 
 # Load data
-file_path = "τραγούδια-Ελλάδα.csv"
-xls = pd.ExcelFile(file_path)
-df = pd.read_csv(file_path, sep=";")
+df = pd.read_csv("τραγούδια-Ελλάδα.csv", sep=";")
 
 # Convert year column to numeric format
 df['Χρόνος'] = pd.to_numeric(df['Ετος κυκλοφ..1'], errors='coerce')
